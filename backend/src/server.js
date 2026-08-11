7b3e8b831600720ab53b9e22e0e2589ac20f252e
kento1811 <khoaking0909@gmail.com>
2026-08-11 23:28:19 +0700

    chang cors origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://fullstackappfrontend-6ky8jro02-khoa25.vercel.app/",
     credentials: true
 }));
 app.use(express.json());
